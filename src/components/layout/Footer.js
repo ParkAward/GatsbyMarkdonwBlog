b94f048321d34cc0b20ffd895a61eed6e1a110aa
Park Award <parksangjun1363@gmail.com>
2022-06-14 22:49:21 +0900

    Footer 텍스트 변경

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,6 +2,6 @@ import React from 'react'
 import { Box } from '@chakra-ui/react/'
 export default function Footer() {
   return (
-    <Box bottom={0} p={50} borderTop={"1px solid black"} mt={20}>Park Award @ power By Gatsby</Box>
+    <Box bottom={0} p={50} borderTop={"1px solid black"} mt={20}>Park Award @ powered By Gatsby</Box>
   )
 }
