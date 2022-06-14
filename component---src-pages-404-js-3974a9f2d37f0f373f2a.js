2176e088a544bb147439436912671c8ab594eda3
Park Award <parksangjun1363@gmail.com>
2022-06-14 22:58:25 +0900

    Updates

---
+++ component---src-pages-404-js-3974a9f2d37f0f373f2a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkroot_p_blog=self.webpackChunkroot_p_blog||[]).push([[883],{429:function(t,e,a){a.r(e);var n=a(7294),o=a(1825),l=a(262);e.default=function(t){var e=t.data,a=t.location,u=e.site.siteMetadata.title;return n.createElement(o.Z,{location:a,title:u},n.createElement(l.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-3974a9f2d37f0f373f2a.js.map
